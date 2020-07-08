a1769d6404e9ca7335914431de2f3e82687b4490
hanjian <hanjian2018@126.com>
2020-07-09 01:22:40 +0800

    hello

---
+++ base/hello/hello.go
@@ -0,0 +1,11 @@
+// 声明包
+package main
+
+// 倒入
+import "fmt"
+
+// 程序的入口方法
+func main() {
+	// 输出hello world
+	fmt.Println("Hello World")
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,6 @@
 # GO 语言学习历程
+
+- [Hello World](_posts/hello.md)
+- [数据类型](_posts/dataype.md)
+
+--------end--------
